1010697daaf86af98b1fa5588dbd4d74c6c072aa
HARLEY COOKE <101873070@student.swin.edu.au>
2019-06-18 11:43:11 +1000

    Update

---
+++ Dip-Seminar-Delegates-Lambda-Linq_Exercises-master/Delegate_Exercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using FileParserNetStandard;
+
+public delegate List<List<string>> Parser(List<List<string>> data);
+
+namespace Delegate_Exercise {
+ 
+    
+    internal class Delegate_Exercise {
+
+        public static string csvPath = "C:/Users/Owner/Documents/Diploma Weekly Task 11/Dip-Seminar-Delegates-Lambda-Linq_Exercises-master/Files/myData.csv";
+        public static string tsvPath = "C:/Users/Owner/Documents/Diploma Weekly Task 11/Dip-Seminar-Delegates-Lambda-Linq_Exercises-master/Files/data.tsv";
+        public static string writeFile = "C:/Users/Owner/Documents/Diploma Weekly Task 11/Dip-Seminar-Delegates-Lambda-Linq_Exercises-master/Files/dataWrite.txt";
+        public static void Main(string[] args) {
+
+            FileHandler fh = new FileHandler();
+            DataParser dp = new DataParser();
+            List<string> newFile = fh.ReadFile(csvPath);
+            List<List<string>> Kellogs = fh.ParseCsv(newFile);
+
+            Func<List<List<string>>, List<List<string>>> dataHandler = new Func<List<List<string>>, List<List<string>>>(dp.StripWhiteSpace);
+            dataHandler += dp.StripHashtags;
+            dataHandler += dp.StripQuotes;
+
+            Kellogs = dataHandler(Kellogs);
+
+            fh.WriteFile(writeFile, ',', Kellogs);
+          
+
+        }   
+    }
+}
